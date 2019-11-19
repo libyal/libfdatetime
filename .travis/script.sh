b5d944edf30d54d27561b6c058f6b59b7b731565
Joachim Metz <joachim.metz@gmail.com>
2019-11-19 07:43:33 +0100

    Applied updates

--- .travis/script.sh
+++ .travis/script.sh
@@ -25,7 +25,7 @@ then
 
 		make install DESTDIR=${PWD}/osx-pkg;
 		mkdir -p ${PWD}/osx-pkg/usr/share/doc/libfdatetime;
-		cp AUTHORS COPYING NEWS README ${PWD}/osx-pkg/usr/share/doc/libfdatetime;
+		cp AUTHORS COPYING COPYING.LESSER NEWS README ${PWD}/osx-pkg/usr/share/doc/libfdatetime;
 
 		pkgbuild --root osx-pkg --identifier com.github.libyal.libfdatetime --version ${VERSION} --ownership recommended ../libfdatetime-${VERSION}.pkg;
 	fi
