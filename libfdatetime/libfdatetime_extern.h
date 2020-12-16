14ef440cefb7e9e9390d73c55c9539bf6672231e
Joachim Metz <joachim.metz@gmail.com>
2020-12-16 17:16:53 +0100

    Applied updates

--- libfdatetime/libfdatetime_extern.h
+++ libfdatetime/libfdatetime_extern.h
@@ -30,7 +30,7 @@
 
 #include <libfdatetime/extern.h>
 
-#if defined( __CYGWIN__ )
+#if defined( __CYGWIN__ ) || defined( __MINGW32__ )
 #define LIBFDATETIME_EXTERN_VARIABLE	extern
 #else
 #define LIBFDATETIME_EXTERN_VARIABLE	LIBFDATETIME_EXTERN
