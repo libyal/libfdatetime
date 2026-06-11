21061e5c59c180cce426089b84c21200c1fd1805
Joachim Metz <joachim.metz@gmail.com>
2026-06-11 18:05:21 +0200

    Replaced test scripts with autotest

--- libfdatetime/libfdatetime_extern.h
+++ libfdatetime/libfdatetime_extern.h
@@ -24,6 +24,19 @@
 
 #include <common.h>
 
+#if !defined( __CYGWIN__ ) && !defined( _WIN32 ) && defined( __has_attribute )
+#if __has_attribute( visibility )
+#define LIBFDATETIME_INTERNAL	__attribute__((visibility("hidden"))) extern
+
+#else
+#define LIBFDATETIME_INTERNAL	extern
+
+#endif /* __has_attribute( visibility ) */
+#else
+#define LIBFDATETIME_INTERNAL	extern
+
+#endif /* !defined( __CYGWIN__ ) && !defined( _WIN32 ) && defined( __has_attribute ) */
+
 /* Define HAVE_LOCAL_LIBFDATETIME for local use of libfdatetime
  */
 #if !defined( HAVE_LOCAL_LIBFDATETIME )
@@ -32,7 +45,7 @@
 
 #else
 #define LIBFDATETIME_EXTERN		/* extern */
-#define LIBFDATETIME_EXTERN_VARIABLE	extern
+#define LIBFDATETIME_EXTERN_VARIABLE	LIBFDATETIME_INTERNAL
 
 #endif /* !defined( HAVE_LOCAL_LIBFDATETIME ) */
 
--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20201121
+# Version: 20260609
 
-if test -f ${PWD}/libfdatetime/.libs/libfdatetime.1.dylib && test -f ./pyfdatetime/.libs/pyfdatetime.so;
+if test -f ${PWD}/libfdatetime/.libs/libfdatetime.1.dylib && test -f ./pyfdatetime/.libs/pyfdatetime.so
 then
-	install_name_tool -change /usr/local/lib/libfdatetime.1.dylib ${PWD}/libfdatetime/.libs/libfdatetime.1.dylib ./pyfdatetime/.libs/pyfdatetime.so;
+	install_name_tool -change /usr/local/lib/libfdatetime.1.dylib ${PWD}/libfdatetime/.libs/libfdatetime.1.dylib ./pyfdatetime/.libs/pyfdatetime.so
 fi
 
-make check CHECK_WITH_STDERR=1;
-RESULT=$?;
+make check-build > /dev/null
 
-if test ${RESULT} -ne 0 && test -f tests/test-suite.log;
+make check $@
+RESULT=$?
+
+if test ${RESULT} -ne 0
 then
-	cat tests/test-suite.log;
+	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
-exit ${RESULT};
+exit ${RESULT}
 
